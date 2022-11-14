cd0b04305bb3cc5ba935826146cee0fda2d844c6
fraportidl <fraportidl@gmail.com>
2022-11-14 14:09:44 -0400

    funcaoConstrutura

--- funcaoConstrutora.js
+++ funcaoConstrutora.js
@@ -2,4 +2,9 @@ function Carro (velocidadeMaximo = 200, delta =5) {
     // atributo privado
     let _velocidadeAtual = 0
     
+    // metodo publico
+    this.acelerar = function () {
+        if(velocidadeAtual + delta <= velocidadeMaximo)
+    }
 }
+
